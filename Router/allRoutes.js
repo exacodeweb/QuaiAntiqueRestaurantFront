3d6318ca0488f4aa623326c8aa9ceaa68e41e444
Franck Giet <franck.giet@gmail.com>
2025-07-10 08:05:35 +0200

    Ajout de la page galerie

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,6 +3,7 @@ import Route from "./Route.js"; // Ce fichier contient toute les routes et impor
 //Définir ici vos routes
 export const allRoutes = [ // Toute les routes sont définie dans ce tableaux
     new Route("/", "Accueil", "/pages/home.html"),/*new Route("/adresse du site",*/
+    new Route("/galerie", "La galerie", "/pages/galerie.html"),
     /*new Route("/Galerie", "Galerie", "/pages/Galerie.html", "/js/galerie.js"),*/ //ligne pour ajouter une nouvelle page
   ];
 
