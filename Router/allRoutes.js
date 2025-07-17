c2751f7eb938fea7d21f7be748d222f289f51e78
Franck Giet <franck.giet@gmail.com>
2025-07-17 17:33:50 +0200

    Gestion de l'affichage des réservations

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -8,7 +8,9 @@ export const allRoutes = [ // Toute les routes sont définie dans ce tableaux
     new Route("/signin", "Connexion", "/pages/auth/signin.html"),
     new Route("/signup", "Inscription", "/pages/auth/signup.html"),
     new Route("/account", "Mon compte", "/pages/auth/account.html"),
-        new Route("/editPassword", "Changement de mot de passe", "/pages/auth/editPassword.html"),
+    new Route("/editPassword", "Changement de mot de passe", "/pages/auth/editPassword.html"),
+    new Route("/allResa", "Vos réservations", "/pages/reservations/allResa.html"),
+  
   ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
