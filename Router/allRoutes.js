00391b88e153b86c2347f314e5097cde8f2efd57
Franck Giet <franck.giet@gmail.com>
2025-07-17 12:11:24 +0200

    ajout des pages Connexion, Inscription et Mon compte

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -5,6 +5,10 @@ export const allRoutes = [ // Toute les routes sont définie dans ce tableaux
     new Route("/", "Accueil", "/pages/home.html"),/*new Route("/adresse du site",*/
     new Route("/galerie", "La galerie", "/pages/galerie.html"),
     /*new Route("/Galerie", "Galerie", "/pages/Galerie.html", "/js/galerie.js"),*/ //ligne pour ajouter une nouvelle page
+    new Route("/signin", "Connexion", "/pages/auth/signin.html"),
+    new Route("/signup", "Inscription", "/pages/auth/signup.html"),
+    new Route("/account", "Mon compte", "/pages/auth/account.html"),
+        new Route("/editPassword", "Changement de mot de passe", "/pages/auth/editPassword.html"),
   ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
