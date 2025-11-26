fe5255a00912b9ebb5d7d52ca08a71529935661b
Franck Giet <franck.giet@gmail.com>
2025-11-26 22:27:46 +0100

    refactor(router): réorganisation et améliorations du système de routing

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,18 +2,19 @@ import Route from "./Route.js"; // Ce fichier contient toute les routes et impor
 
 //Définir ici vos routes
 export const allRoutes = [ // Toute les routes sont définie dans ce tableaux
-    new Route("/", "Accueil", "/pages/home.html", []),/*new Route("/adresse du site",*/
-    new Route("/galerie", "La galerie", "/pages/galerie.html", []),
-    /*new Route("/Galerie", "Galerie", "/pages/Galerie.html", "/js/galerie.js"),*/ //ligne pour ajouter une nouvelle page
-    new Route("/signin", "Connexion", "/pages/auth/signin.html", ["disconnected"], "/js/auth/signin.js"),
-    /*new Route("/signup", "Inscription", "/pages/auth/signup.html"),*/
-    //new Route("/signup", "Inscription", "/pages/auth/signup.html"),//, "/js/auth/signup.js"),
-    new Route("/signup", "Inscription", "/pages/auth/signup.html", ["disconnected"], "/js/auth/signup.js"),
-    new Route("/account", "Mon compte", "/pages/auth/account.html", ["client", "admin"]),
-    new Route("/editPassword", "Changement de mot de passe", "/pages/auth/editPassword.html", ["client", "admin"]),
-    new Route("/allResa", "Vos réservations", "/pages/reservations/allResa.html", ["client"]),
-    new Route("/reserver", "Réserver", "/pages/reservations/reserver.html", ["client"]),
-  ];
+  // Page accueil
+  new Route("/", "Accueil", "/pages/home.html", []),//, [] new Route("/adresse du site",*/ //leçons: auth routage []
+  // Page Galerie
+  new Route("/galerie", "La galerie", "/pages/galerie.html", []),//, [] " /js/galerie.js"  //leçons: auth routage []
+  // Page connexion
+  new Route("/signin", "Connexion", "/pages/auth/signin.html", ["disconnected"], "/js/auth/signin.js"),//leçons: dynamiser la parti front:, "/js/auth/signin.js" //  //leçons: auth routage []
+  new Route("/signup", "Inscription", "/pages/auth/signup.html", ["disconnected"], "/js/auth/signup.js"),//leçons: champs requis // //leçons: auth routage []
+  new Route("/account", "Mon compte", "/pages/auth/account.html", ["client", "admin"]), //leçons: auth routage []
+  new Route("/editPassword", "Changement de mot de passe", "/pages/auth/editPassword.html", ["client", "admin"]), //leçons: auth routage []
+  // Page réservations
+  new Route("/allResa", "Vos réservations", "/pages/reservations/allResa.html", ["client"]), //leçons: auth routage [] // uniquement accessible au utiltsateur connecter
+  new Route("/reserver", "Réserver", "/pages/reservations/reserver.html", ["client"]), //leçons: auth routage []
+];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Quai Antique";
--- Router/router.js
+++ Router/router.js
@@ -1,102 +1,89 @@
-import Route from "./Route.js"; // ce fichier contient toute la logique de routage
-import { allRoutes, websiteName } from "./allRoutes.js";
+import Route from "./Route.js";
+import {allRoutes, websiteName} from "./allRoutes.js";
 
 // Création d'une route pour la page 404 (page introuvable)
-const route404 = new Route("404", "Page introuvable", "/pages/404.html", []);
+const route404 = new Route("404", "Page introuvable", "/pages/404.html", []);//leçons: auth routage []
 
 // Fonction pour récupérer la route correspondant à une URL donnée
-const getRouteByUrl = (url) => {
-  let currentRoute = null;
+const getRouteByUrl = (url) => {let currentRoute = null;
+// Parcours de toutes les routes pour trouver la correspondance
+allRoutes.forEach((element) => {
+if(element.url == url) {
+currentRoute = element;
+}
+});
 
-  // Parcours de toutes les routes pour trouver la correspondance
-  allRoutes.forEach((element) => {
-    if (element.url == url) {
-      currentRoute = element;
-    }
-  });
-  // Si aucune correspondance n'est trouvée, on retourne la route 404
-  if (currentRoute != null) {
-    return currentRoute;
-  } else {
-    return route404;
-  }
+// Si aucune correspondance n'est trouvée, on retourne la route 404
+if(currentRoute != null) {
+return currentRoute;
+}else{
+  return route404;
+}
 };
 
 // Fonction pour charger le contenu de la page
-const LoadContentPage = async () => {
-  const path = window.location.pathname;
+const LoadContentPage = async() => {
+const path = window.location.pathname;
+// Récupération de l'URL actuelle
+const actualRoute = getRouteByUrl(path);
 
-  // Récupération de l'URL actuelle
-  const actualRoute = getRouteByUrl(path);
-
-  // vérifier les droits d'accès à la page
-  const allRolesArray = actualRoute.authorize;
-
-  if(allRolesArray.length > 0){
-    if(allRolesArray.includes("disconnected")){
-      if(isConnected()){
-        window.location.replace("/");
-      }
-    }
-    else{
-      const roleUser = getRole()
-      if(!allRolesArray.includes(roleUser)){
-        window.location.replace("/");
-      }
+// vérifier les droits d'access à la page
+const allRolesArray = actualRoute.authorize;
+// verification uniquement si la taille du tableau est > 0
+if (allRolesArray.length > 0){//verification si tableau est > 0
+  if (allRolesArray.includes("disconnected")){// verifie si utilisateur disconnected
+    if(isConnected()){// si connecter on redirige
+      alert("Vous êtes déjà connecté !");//Test
+      window.location.replace("/"); // ex: on peut redirigé sur une page de connexion
     }
   }
-
-  // Récupération du contenu HTML de la route
-  const html = await fetch(actualRoute.pathHtml).then((data) => data.text());
-
-  // Ajout du contenu HTML à l'élément avec l'ID "main-page"
-  document.getElementById("main-page").innerHTML = html;
-
-  // Ajout du contenu JavaScript
-  if (actualRoute.pathJS != "") { /*modifier pathJS en pathjs*/
-    // Création d'une balise script
-    var scriptTag = document.createElement("script");
-    scriptTag.setAttribute("type", "text/javascript");
-    scriptTag.setAttribute("src", actualRoute.pathJS);/*modifiaction de pathJS en pathj*/
-
-    // Ajout de la balise script au corps du document
-    document.querySelector("body").appendChild(scriptTag);
+  else {//si le tableau ne contient pas le role déconnecté
+    const roleUser = getRole();//verifier le role de l'utilisateur, on recupere le role de l'utilisateur "client ou admin"
+    //const userConnected = isConnected();//correctif bug
+    if(!allRolesArray.includes(roleUser)){// verifier si le tableau contien cet élément, si il ne le contient pas alors redirection
+    //if (!userConnected || !allRolesArray.includes(roleUser)) { //correctif bug
+      alert("Accès refusé. Connectez-vous pour continuer.");//Test
+      window.location.replace("/"); //une redirection sur un page pour signalé que l'utilisateur n'a pas les droits d'access par exemple
+    }
   }
+}
 
-  // Changement du titre de la page
-  document.title = actualRoute.title + " - " + websiteName;
+// Récupération du contenu HTML de la route
+const html = await fetch(actualRoute.pathHtml).then((data) => data.text());
+// Ajout du contenu HTML à l'élément avec l'ID "main-page"
+document.getElementById("main-page").innerHTML = html;
 
-  // affiché et masquer les éléments en fonction du rôle
-  showAndHideElementsForRoles();//méthode
+// Ajout du contenu JavaScript
+if (actualRoute.pathJS != "") {
+// Création d'une balise script
+var scriptTag = document.createElement("script");
+scriptTag.setAttribute("type","text/javascript");
+scriptTag.setAttribute("src",actualRoute.pathJS);
 
+// Ajout de la balise script au corps du document
+document.querySelector("body").appendChild(scriptTag);
+}
+
+// Changement du titre de la page
+document.title = actualRoute.title + " - " + websiteName;
+
+//afficher et masquer les éléments en fonction du rôle //leçons afficher/masquer
+ showAndHideElementsForRoles();
 };
 
 // Fonction pour gérer les événements de routage (clic sur les liens)
 const routeEvent = (event) => {
-  event = event || window.event;
-  event.preventDefault();
-
-  // Mise à jour de l'URL dans l'historique du navigateur
-  window.history.pushState({}, "", event.target.href);
-
-  // Chargement du contenu de la nouvelle page
-  LoadContentPage();
+event = event || window.event;
+event.preventDefault();
+// Mise à jour de l'URL dans l'historique du navigateur
+window.history.pushState({}, "", event.target.href);
+// Chargement du contenu de la nouvelle page
+LoadContentPage();
 };
 
 // Gestion de l'événement de retour en arrière dans l'historique du navigateur
 window.onpopstate = LoadContentPage;
-
 // Assignation de la fonction routeEvent à la propriété route de la fenêtre
 window.route = routeEvent;
-
 // Chargement du contenu de la page au chargement initial
 LoadContentPage();
-
-
-//-----------------------------------------------------------------------------------------
-
-
-
-
-
-
